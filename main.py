facf6ede2a388f282f499e88f9ddac3a4d126698
terminalmax <nishanthrao9@gmail.com>
2022-06-21 10:21:17 +0530

    Loaded strings appear in textbox

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class ApplicationWindow(QMainWindow):
         self.plaintext = ""
         self.ciphertext = ""
 
+        #Connecting buttons to functions
         self.LoadPlaintextButton.clicked.connect(self.loadplaintext)
         self.LoadCiphertextButton.clicked.connect(self.loadciphertext)
 
@@ -25,6 +26,9 @@ class ApplicationWindow(QMainWindow):
         self.DecryptButton.clicked.connect(self.decryptfunction)
         self.EncryptButton.clicked.connect(self.encryptfunction)
 
+        self.KeyInput.setPlaceholderText("AAAAAAAAAAAAAA")
+
+    #Abstracting error dialog
     def showErrorDialog(self, title, content):
         dlg = QMessageBox(self)
         dlg.setWindowTitle(title)
@@ -32,10 +36,14 @@ class ApplicationWindow(QMainWindow):
 
         dlg.exec()
 
+    #Loading functions
     def loadplaintext(self):
         
         fname = QFileDialog.getOpenFileName(self, 'Choose Plain Text', directory='', filter='Text files (*.txt)')
-         
+        
+        if fname[0] == '':
+            return
+
         try:
             with open(fname[0], 'r') as fp:
                 self.plaintext = fp.read()
@@ -48,40 +56,47 @@ class ApplicationWindow(QMainWindow):
             print("Error: Could not load file")
             return
 
+        #Setting plain text in text box
         print(self.plaintext)
+        self.PlaintextInput.setPlainText(self.plaintext)
     
     def loadciphertext(self):
 
         fname = QFileDialog.getOpenFileName(self, 'Choose Plain Text', directory='', filter='Text files (*.txt)')
         
+        if fname[0] == '':
+            return
+
         try:
             with open(fname[0], 'r') as fp:
                 self.ciphertext = fp.read()
         except FileNotFoundError:
-            
             self.showErrorDialog("File Error", "File not found or invalid path!")
-
             print("File not found error!")
             return
         except:
-
             self.showErrorDialog("File Error", "Unexpected File error has occured!")
-
             print("Error: Could not load file")
             return
             
         print(self.ciphertext)
+        self.CiphertextInput.setPlainText(self.ciphertext)
     
+    #Saving functions
     def saveplaintext(self):
         pass
 
     def saveciphertext(self):
         pass
     
-    
-
     def encryptfunction(self):
+        if self.running == 1:
+            return
+        
         self.running = 1
+
+        
+
         self.running = 0
     
     def decryptfunction(self):
